7f627c6f963ee75261573a4b108735860e9c40d0
JoaoPLdeSouza <141786327+JoaoPLdeSouza@users.noreply.github.com>
2025-05-27 14:19:02 -0300

    Ajustes de aparência na página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { FaUser, FaLock } from "react-icons/fa";
+// Importar FaEye e FaEyeSlash
+import { FaUser, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
 import "../assets/Login.css";
 import authService from "../services/authService";
+import myFinanceLogo from "../assets/MyFinance_logo.png"; // Certifique-se de que o caminho está correto!
 
 const Login = () => {
   const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState(""); // precisa ser 'senha', como no backend
+  const [senha, setSenha] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // Novo estado para controlar a visibilidade da senha
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -26,13 +29,19 @@ const Login = () => {
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="login-container">
-      <div className="login-image-container"></div>
+      <div className="login-image-container">
+        <img src={myFinanceLogo} alt="MyFinance Logo" className="logo-image" />
+      </div>
       <h2 className="login-title">Login</h2>
       <form onSubmit={handleSubmit}>
         <div id="email-group" className="input-group">
-          <label htmlFor="email">E-mail</label>
+          <label htmlFor="email">E-mail</label> {/* Label movida para fora do input-wrapper */}
           <div className="input-wrapper">
             <FaUser className="icon" />
             <input
@@ -46,17 +55,23 @@ const Login = () => {
           </div>
         </div>
         <div id="senha-group" className="input-group">
-          <label htmlFor="senha">Senha</label>
+          <label htmlFor="senha">Senha</label> {/* Label movida para fora do input-wrapper */}
           <div className="input-wrapper">
             <FaLock className="icon" />
             <input
-              type="password"
+              type={showPassword ? "text" : "password"} // Tipo de input dinâmico
               id="senha"
               className="input-field"
               value={senha}
               onChange={(e) => setSenha(e.target.value)}
               required
             />
+            {/* Ícone de olho para alternar a visibilidade da senha */}
+            {showPassword ? (
+              <FaEyeSlash className="toggle-password" onClick={togglePasswordVisibility} />
+            ) : (
+              <FaEye className="toggle-password" onClick={togglePasswordVisibility} />
+            )}
           </div>
         </div>
 
